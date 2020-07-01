c0133ba58895fca9848b48413f1cab2081eb7d82
hugoalh <32359235+hugoalh@users.noreply.github.com>
2020-07-01 16:02:10 +0800

    Fix `isUndefined` logic

--- isundefined.js
+++ isundefined.js
@@ -7,14 +7,14 @@ function isUndefined(item, fuzzyMode = false) {
 	if (typeof fuzzyMode != "boolean") {
 		throw new TypeError(`Invalid type of "fuzzyMode"! Require type of boolean.`);
 	};
-	if (typeof item != "undefined") {
-		return false;
+	if (typeof item == "undefined") {
+		return true;
 	};
 	if (fuzzyMode == true) {
 		if (item === "undefined") {
 			return true;
 		};
 	};
-	return true;
+	return false;
 };
 module.exports = isUndefined;
