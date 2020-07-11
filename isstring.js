e8b5e012d6d9af8b0006acbe977bd7eecdd52ab9
hugoalh <32359235+hugoalh@users.noreply.github.com>
2020-07-11 12:38:48 +0800

    Another tweak function

--- isstring.js
+++ isstring.js
@@ -14,11 +14,11 @@ const isJSON = require("./isjson.js");
  * @returns {(boolean|null)} Determine result.
  */
 function isString(item, configuration) {
-	let fuzzyMode = false;
+	let modeFuzzy = false;
 	if (isJSON(configuration) == true) {
 		if (configuration.fuzzyMode) {
 			if (typeof configuration.fuzzyMode == "boolean") {
-				fuzzyMode = configuration.fuzzyMode;
+				modeFuzzy = configuration.fuzzyMode;
 			} else {
 				console.warn(`Invalid type of "configuration.fuzzyMode"! Require type of boolean. Ignored this parameter.`);
 			};
@@ -30,7 +30,7 @@ function isString(item, configuration) {
 	if (item.length == 0) {
 		return null;
 	};
-	if (fuzzyMode == true) {
+	if (modeFuzzy == true) {
 		if (item === "null") {
 			return null;
 		};
--- isundefined.js
+++ isundefined.js
@@ -14,11 +14,11 @@ const isJSON = require("./isjson.js");
  * @returns {boolean} Determine result.
  */
 function isUndefined(item, configuration) {
-	let fuzzyMode = false;
+	let modeFuzzy = false;
 	if (isJSON(configuration) == true) {
 		if (configuration.fuzzyMode) {
 			if (typeof configuration.fuzzyMode == "boolean") {
-				fuzzyMode = configuration.fuzzyMode;
+				modeFuzzy = configuration.fuzzyMode;
 			} else {
 				console.warn(`Invalid type of "configuration.fuzzyMode"! Require type of boolean. Ignored this parameter.`);
 			};
@@ -27,7 +27,7 @@ function isUndefined(item, configuration) {
 	if (typeof item == "undefined") {
 		return true;
 	};
-	if (fuzzyMode == true) {
+	if (modeFuzzy == true) {
 		if (item === "undefined") {
 			return true;
 		};
