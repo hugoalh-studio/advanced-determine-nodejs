423239377d6430bb2a6c107eac0d538a06496784
hugoalh <hugoalh@gmail.com>
2020-07-14 17:17:16 +0800

    Version 2.0.2 (#8)
    - Code polish
    - Rename parameter

--- isboolean.js
+++ isboolean.js
@@ -8,25 +8,27 @@ const isJSON = require("./isjson.js");
  * @function isBoolean
  * @description Determine item is type of boolean or not.
  * @param {*} item Item that need to determine.
- * @param {object} [configuration] Configuration.
- * @param {boolean} [configuration.fuzzyMode=false] Enable/Disable fuzzy mode.
+ * @param {object} [option] Option.
+ * @param {boolean} [option.fuzzyMode=false] Enable/Disable fuzzy mode.
  * @returns {boolean} Determine result.
  */
-function isBoolean(item, configuration) {
-	let fuzzyMode = false;
-	if (isJSON(configuration) == true) {
-		if (configuration.fuzzyMode) {
-			if (typeof configuration.fuzzyMode == "boolean") {
-				fuzzyMode = configuration.fuzzyMode;
+function isBoolean(item, option) {
+	let runtime = {
+		fuzzyMode: false
+	};
+	if (isJSON(option) == true) {
+		if (option.fuzzyMode) {
+			if (typeof option.fuzzyMode == "boolean") {
+				runtime.fuzzyMode = option.fuzzyMode;
 			} else {
-				console.warn(`Invalid type of "configuration.fuzzyMode"! Require type of boolean. Ignored this parameter.`);
+				console.warn(`Invalid type of "option.fuzzyMode"! Require type of boolean. Ignored this parameter.`);
 			};
 		};
 	};
 	if (typeof item == "boolean") {
 		return true;
 	};
-	if (fuzzyMode == true) {
+	if (runtime.fuzzyMode == true) {
 		if (item === "true" || item === "false") {
 			return true;
 		};
