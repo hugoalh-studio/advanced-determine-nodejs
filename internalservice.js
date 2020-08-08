14d6335a9181390a05fc3e12e6aa031ed3bae19e
hugoalh <32359235+hugoalh@users.noreply.github.com>
2020-08-08 14:00:59 +0800

    Add module map

--- internalservice.js
+++ internalservice.js
@@ -5,6 +5,68 @@
 ==================*/
 const configuration = require("./configuration.js");
 /**
+ * @const {object} moduleMap
+ */
+const moduleMap = {
+	"arr": "array",
+	"array": "array",
+	"boolean": "boolean",
+	"buf": "buffer",
+	"buffer": "buffer",
+	"date": "date",
+	"json": "json",
+	"nul": "null",
+	"null": "null",
+	"num": "number",
+	"number": "number",
+	"numberfloat": "numberfloat",
+	"numbernegative": "numbernegative",
+	"numbernegativefloat": "numbernegativefloat",
+	"numbernegativeinteger": "numbernegativeinteger",
+	"numbernegativesafefloat": "numbernegativesafefloat",
+	"numbernegativesafeinteger": "numbernegativesafeinteger",
+	"numberpositive": "numberpositive",
+	"numberpositivefloat": "numberpositivefloat",
+	"numberpositiveinteger": "numberpositiveinteger",
+	"numberpositivesafefloat": "numberpositivesafefloat",
+	"numberpositivesafeinteger": "numberpositivesafeinteger",
+	"numbersafefloat": "numbersafefloat",
+	"numflt": "numberfloat",
+	"numngt": "numbernegative",
+	"numngtflt": "numbernegativefloat",
+	"numngtint": "numbernegativeinteger",
+	"numngtsflt": "numbernegativesafefloat",
+	"numngtsint": "numbernegativesafeinteger",
+	"numpst": "numberpositive",
+	"numpstflt": "numberpositivefloat",
+	"numpstint": "numberpositiveinteger",
+	"numpstsflt": "numberpositivesafefloat",
+	"numpstsint": "numberpositivesafeinteger",
+	"numsflt": "numbersafefloat",
+	"obj": "object",
+	"object": "object",
+	"objectpair": "objectpair",
+	"objectplain": "objectpair",
+	"objpair": "objectpair",
+	"objplain": "objectpair",
+	"regex": "regularexpression",
+	"regexp": "regularexpression",
+	"regexr": "regularexpression",
+	"regularexpression": "regularexpression",
+	"str": "string",
+	"strascii": "stringascii",
+	"string": "string",
+	"stringascii": "stringascii",
+	"stringifyjson": "stringifyjson",
+	"stringlowercase": "stringlowercase",
+	"stringuppercase": "stringuppercase",
+	"strjson": "stringifyjson",
+	"strl": "stringlowercase",
+	"stru": "stringuppercase",
+	"udf": "undefined",
+	"undefined": "undefined"
+};
+/**
  * @function generalError
  * @param {string} message
  * @returns {undefined}
@@ -52,6 +114,7 @@ function typeError(message) {
 	};
 	throw new TypeError(message);
 };
+module.exports.moduleMap = moduleMap;
 module.exports.generalError = generalError;
 module.exports.rangeError = rangeError;
 module.exports.referenceError = referenceError;
