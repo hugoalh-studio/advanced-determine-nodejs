bbf36203dd601fafc1668ee8a13e5e2f47bf813b
hugoalh <32359235+hugoalh@users.noreply.github.com>
2020-12-09 17:59:04 +0800

    Update DTS

--- lib/typeof.d.ts
+++ lib/typeof.d.ts
@@ -1,2 +1,8 @@
 type ReturnTypeOf = "array" | "bigint" | "boolean" | "dataview" | "date" | "function" | "map" | "nan" | "null" | "number" | "object" | "regexp" | "set" | "string" | "symbol" | "undefined" | "weakmap" | "weakset" | undefined;
+/**
+ * @function typeOf
+ * @description Determine item type of the unevaluated operand.
+ * @param {*} item Item that need to determine.
+ * @returns {(string|undefined)} Determine result.
+ */
 export function typeOf(item: any): ReturnTypeOf;
