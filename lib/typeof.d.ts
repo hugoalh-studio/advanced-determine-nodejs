2c1e2d26d5fe7c5215825089294f8d78a2e3a0f1
hugoalh <32359235+hugoalh@users.noreply.github.com>
2021-06-10 18:55:49 +0800

    -

--- lib/typeof.d.ts
+++
@@ -1,8 +0,0 @@
-type ReturnTypeOf = "array" | "bigint" | "boolean" | "dataview" | "date" | "function" | "map" | "nan" | "null" | "number" | "object" | "regexp" | "set" | "string" | "symbol" | "undefined" | "weakmap" | "weakset" | undefined;
-/**
- * @function typeOf
- * @description Determine item type of the unevaluated operand.
- * @param {*} item Item that need to determine.
- * @returns {ReturnTypeOf} Determine result.
- */
-export function typeOf(item: any): ReturnTypeOf;
