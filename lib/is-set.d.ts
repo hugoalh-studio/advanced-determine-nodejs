e91cff04987131e4050e121408c4ff436f6f84f7
hugoalh <32359235+hugoalh@users.noreply.github.com>
2022-11-02 17:35:47 +0800

    20221102A

--- lib/is-set.d.ts
+++ lib/is-set.d.ts
@@ -4,7 +4,7 @@ export = isSet;
  * @description Determine item is type of set or not.
  * @param {unknown} item Item that need to determine.
  * @param {object} [param1={}] Options.
- * @param {boolean} [param1.allowEmpty] Whether to allow an empty set.
+ * @param {boolean} [param1.allowEmpty=false] Whether to allow an empty set.
  * @param {number} [param1.maximumSize=Infinity] Maximum size of the set.
  * @param {number} [param1.minimumSize=0] Minimum size of the set.
  * @returns {item is Set} Determine result.
--- lib/is-string.d.ts
+++ lib/is-string.d.ts
@@ -5,7 +5,7 @@ export = isString;
  * @description Determine item is type of string or not.
  * @param {unknown} item Item that need to determine.
  * @param {object} [param1={}] Options.
- * @param {boolean} [param1.allowEmpty] Whether to allow an empty string.
+ * @param {boolean} [param1.allowEmpty=false] Whether to allow an empty string.
  * @param {boolean} [param1.ascii] Whether an ASCII string.
  * @param {boolean} [param1.lowerCase] Whether a lower case string.
  * @param {number} [param1.maximumLength=Infinity] Maximum length of the string.
