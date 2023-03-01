761be9473b3cac8794c633f7d3fabb8dc1c57146
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-03-01 16:43:55 +0800

    Add integer type 128

--- lib/internal/integer-types-range.d.ts
+++ lib/internal/integer-types-range.d.ts
@@ -1,23 +1,16 @@
 export default integerTypesRange;
 /**
- * Whether to return result as type of number.
- */
-export type AsNumber = boolean;
-/**
  * @access private
  * @function integerTypesRange
  * @param {string} name Name of the integer type.
- * @param {AsNumber} [asNumber=false] Whether to return result as type of number.
+ * @param {boolean} [asNumber=false] Whether to return result as type of number.
  * @returns {ReturnType<typeof integerTypesRangeOutput>}
  */
-declare function integerTypesRange(name: string, asNumber?: AsNumber): ReturnType<typeof integerTypesRangeOutput>;
-/**
- * @typedef {boolean} AsNumber Whether to return result as type of number.
- */
+declare function integerTypesRange(name: string, asNumber?: boolean): ReturnType<typeof integerTypesRangeOutput>;
 /**
  * @access private
  * @function integerTypesRangeOutput
- * @template {AsNumber} T
+ * @template {boolean} T
  * @param {[bigint, bigint]} output Output.
  * @param {T} [asNumber=false] Whether to return result as type of number.
  * @returns {T extends true ? [number, number] : [bigint, bigint]}
