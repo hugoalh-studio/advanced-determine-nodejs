c272e89f16e888e65a86fa48fbcb432a74530daa
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-03-06 16:32:52 +0800

    Improve ITR

--- lib/internal/integer-types-range.d.ts
+++ lib/internal/integer-types-range.d.ts
@@ -3,17 +3,7 @@ export default integerTypesRange;
  * @access private
  * @function integerTypesRange
  * @param {string} name Name of the integer type.
- * @param {boolean} [asNumber=false] Whether to return result as type of number.
- * @returns {ReturnType<typeof integerTypesRangeOutput>}
+ * @returns {[bigint, bigint]}
  */
-declare function integerTypesRange(name: string, asNumber?: boolean): ReturnType<typeof integerTypesRangeOutput>;
-/**
- * @access private
- * @function integerTypesRangeOutput
- * @template {boolean} T
- * @param {[bigint, bigint]} output Output.
- * @param {T} [asNumber=false] Whether to return result as type of number.
- * @returns {T extends true ? [number, number] : [bigint, bigint]}
- */
-declare function integerTypesRangeOutput<T extends boolean>(output: [bigint, bigint], asNumber?: T): T extends true ? [number, number] : [bigint, bigint];
+declare function integerTypesRange(name: string): [bigint, bigint];
 //# sourceMappingURL=integer-types-range.d.ts.map
