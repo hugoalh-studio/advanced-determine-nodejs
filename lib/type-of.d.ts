902ef00ebd58d494ce70dbd6b9b4e293e6e19464
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-04-28 18:31:39 +0800

    Bump version

--- lib/type-of.d.ts
+++ lib/type-of.d.ts
@@ -1,8 +1,7 @@
-export type AdvancedTypeOf = "array" | "bigint" | "boolean" | "function" | "nan" | "null" | "number" | "object" | "regexp" | "string" | "symbol" | "undefined";
 /**
  * @function typeOf
  * @description Determine item type of the unevaluated operand.
  * @param {unknown} item Item that need to determine.
- * @returns {AdvancedTypeOf} Determine result.
+ * @returns {"array" | "bigint" | "boolean" | "function" | "nan" | "null" | "number" | "object" | "regexp" | "string" | "symbol" | "undefined"} Determine result.
  */
-export function typeOf(item: unknown): AdvancedTypeOf;
+export function typeOf(item: unknown): "array" | "bigint" | "boolean" | "function" | "nan" | "null" | "number" | "object" | "regexp" | "string" | "symbol" | "undefined";
